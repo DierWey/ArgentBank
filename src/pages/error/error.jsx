6b83b7163e61161df5afa28779a707bf03587239
DierWey <didier.wey@laposte.net>
2026-04-08 14:54:46 +0200

    Feat: Style de la page Error

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -2,13 +2,15 @@ import { NavLink } from "react-router-dom"
 
 function Error() {
     return (
-        <>
-            <h1 className="">Error 404</h1>
-            <h2 className="">Sorry, we can't find the page you're looking for.</h2>
-            <NavLink className="" to="/">
-                <button>Go back to Home</button>
-            </NavLink>
-        </>
+        <main className="main bg-dark error-main">
+            <section className="error-block">
+                <h1 className="error-404">Error 404</h1>
+                <h2 className="error-text">Sorry, we can't find the page you're looking for.</h2>
+                <NavLink className="" to="/">
+                    <button className="error-button">Go back to Home</button>
+                </NavLink>
+            </section>
+        </main>
     )
 }
 
